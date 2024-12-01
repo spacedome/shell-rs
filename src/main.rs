2a5002704a912ecc50afb03ecf823cdf13e8e7ca
spacedome <spacedome@users.noreply.github.com>
2024-12-01 13:54:29 -0500

    invalid command

--- src/main.rs
+++ src/main.rs
@@ -10,4 +10,7 @@ fn main() {
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+
+    // command not found
+    println!("{}: command not found", input.trim())
 }
