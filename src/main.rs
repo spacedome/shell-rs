bbf331701854ab14eee086462d2391a2df6cdb36
spacedome <spacedome@users.noreply.github.com>
2024-12-01 16:48:48 -0500

    add echo w nom parser

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
+pub use nom::bytes::complete::tag;
+pub use nom::IResult;
+use nom::{branch::alt, combinator::value};
 #[allow(unused_imports)]
 use std::io::{self, Write};
+
+#[derive(Clone)]
+enum Command {
+    Exit,
+    Echo,
+}
 
 fn main() {
     loop {
@@ -18,13 +27,31 @@ fn main() {
     }
 }
 
-fn run_command(command: String) -> bool {
+fn run_command(input: String) -> bool {
     // command not found
-    match command.trim() {
-        "exit 0" => true,
-        x => {
-            println!("{}: command not found", x);
+    let maybe_command = parse_input(&input.trim());
+    match maybe_command {
+        Ok(command) => match command {
+            (_, Command::Exit) => true,
+            (rem, Command::Echo) => {
+                println!("{}", rem);
+                false
+            }
+        },
+        Err(_) => {
+            println!("{}: command not found", input.trim());
             false
         }
     }
+}
+
+fn parse_input(input: &str) -> IResult<&str, Command> {
+    //  note that this is really creating a function, the parser for abc
+    //  vvvvv
+    //         which is then called here, returning an IResult<&str, &str>
+    //         vvvvv
+    alt((
+        value(Command::Exit, tag("exit 0")),
+        value(Command::Echo, tag("echo ")),
+    ))(input)
 }
