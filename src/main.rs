f42768d28dce9178627d1ee71a52edda9f19955c
spacedome <spacedome@users.noreply.github.com>
2024-12-01 22:27:41 -0500

    cd

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,12 @@ pub use nom::IResult;
 use nom::{branch::alt, multi::many0};
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::str::FromStr;
 
 #[derive(Clone)]
 enum Command<'a> {
-    PWD,
+    Pwd,
+    Cd(std::path::PathBuf),
     Exit(i32),
     Echo(String),
     Type(String),
@@ -38,7 +40,8 @@ fn run_command(input: String) {
             }
             (_, Command::Type(s)) => run_type(&s),
             (_, Command::Bin(s, args)) => run_bin(s, args),
-            (_, Command::PWD) => run_pwd(),
+            (_, Command::Pwd) => run_pwd(),
+            (_, Command::Cd(p)) => run_cd(p),
         },
         Err(_) => {
             println!("{}: command not found", input.trim())
@@ -89,10 +92,21 @@ fn run_bin(path: std::path::PathBuf, args: Vec<&str>) {
 }
 
 fn run_pwd() {
-    match std::env::var("PWD") {
-        Ok(pwd) => println!("{}", pwd),
+    match std::env::current_dir() {
+        Ok(pwd) => println!("{}", pwd.display()),
         Err(_) => println!("error: could not get PWD"),
     };
+}
+
+fn run_cd(path: std::path::PathBuf) {
+    if path.exists() && path.is_dir() {
+        match std::env::set_current_dir(path) {
+            Ok(_) => (),
+            Err(_) => println!("Could not chdir"),
+        }
+    } else {
+        println!("cd: {}: No such file or directory", path.display());
+    }
 }
 
 fn parse_input(input: &str) -> IResult<&str, Command> {
@@ -100,7 +114,8 @@ fn parse_input(input: &str) -> IResult<&str, Command> {
         parse_exit,
         parse_echo,
         parse_type,
-        nom::combinator::value(Command::PWD, nom::combinator::all_consuming(tag("pwd"))),
+        parse_cd,
+        nom::combinator::value(Command::Pwd, nom::combinator::all_consuming(tag("pwd"))),
         parse_bin,
     ))(input)
 }
@@ -139,6 +154,18 @@ fn parse_bin(input: &str) -> IResult<&str, Command> {
         Ok(p) => Ok((input, Command::Bin(p, args))),
         Err(_) => Err(nom::Err::Error(nom::error::Error {
             input: "bin",
+            code: nom::error::ErrorKind::Tag,
+        })),
+    }
+}
+
+fn parse_cd(input: &str) -> IResult<&str, Command> {
+    let (input, _) = tag("cd ")(input)?;
+    let (input, _) = many0(tag(" "))(input)?;
+    match std::path::PathBuf::from_str(input) {
+        Ok(p) => Ok((input, Command::Cd(p))),
+        Err(_) => Err(nom::Err::Error(nom::error::Error {
+            input: "cd",
             code: nom::error::ErrorKind::Tag,
         })),
     }
